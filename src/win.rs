e6ac8c19fde73160bf0849001fbaba22f011a1b6
karin0 <karin0@gmx.com>
2021-05-15 15:09:37 +0800

    Support non-Windows platforms

--- src/win.rs
+++ src/win.rs
@@ -1,48 +1,58 @@
-use log::error;
-use log::info;
-use log::debug;
+#[cfg(windows)]
+mod win {
+    use log::error;
+    use log::info;
+    use log::debug;
+    use std::io;
+    use std::path::PathBuf;
+    use winreg::{RegKey, enums::HKEY_CLASSES_ROOT};
 
-use winreg::enums::HKEY_CLASSES_ROOT;
-use winreg::RegKey;
+    fn get_osu_command() -> io::Result<String> {
+        let it = RegKey::predef(HKEY_CLASSES_ROOT)
+            .open_subkey("osu\\shell\\open\\command")?;
+        it.get_value("")
+    }
 
-use std::io;
-use std::path::PathBuf;
-
-fn get_osu_command() -> io::Result<String> {
-    let it = RegKey::predef(HKEY_CLASSES_ROOT)
-        .open_subkey("osu\\shell\\open\\command")?;
-    it.get_value("")
-}
-
-fn get_osu_path() -> Option<String> {
-    match get_osu_command() {
-        Ok(s) => {
-            debug!("Found in registry: {}", s);
-            match s.chars().next() {
-                Some('"') => {
-                    match s[1..].find('"') {
-                        Some(p) => Some(s[1..p + 1].to_owned()),
-                        _ => None
+    fn get_osu_path() -> Option<String> {
+        match get_osu_command() {
+            Ok(s) => {
+                debug!("Found in registry: {}", s);
+                match s.chars().next() {
+                    Some('"') => {
+                        match s[1..].find('"') {
+                            Some(p) => Some(s[1..p + 1].to_owned()),
+                            _ => None
+                        }
+                    },
+                    _ => match s.find(' ') {
+                        Some(p) => Some(s[0..p].to_owned()),
+                        _ => Some(s)
                     }
-                },
-                _ => match s.find(' ') {
-                    Some(p) => Some(s[0..p].to_owned()),
-                    _ => Some(s)
                 }
+            },
+            Err(e) => {
+                error!("Error reading registy: {:?}", e);
+                None
             }
-        },
-        Err(e) => {
-            error!("Error reading registy: {:?}", e);
-            None
         }
+    }
+
+    pub fn get_songs_path() -> Option<String> {
+        get_osu_path().map(|path| {
+            info!("Found osu! installation at {}", path);
+            let mut path = PathBuf::from(path);
+            path.set_file_name("Songs");
+            path.to_str().unwrap().to_owned()
+        })
     }
 }
 
+#[cfg(windows)]
 pub fn find_songs_path() -> Option<String> {
-    get_osu_path().map(|path| {
-        info!("Found osu! installation at {}", path);
-        let mut path = PathBuf::from(path);
-        path.set_file_name("Songs");
-        path.to_str().unwrap().to_owned()
-    })
+    win::get_songs_path()
+}
+
+#[cfg(not(windows))]
+pub fn find_songs_path() -> Option<String> {
+    None
 }
